2fced52e1ecdd003f62b16718fa53d1ad6079c6c
Wen Jie <wenjie@cavman.com.my>
2018-06-21 16:45:13 +0800

    filter helper 2

--- filter/filter1.js
+++ filter/filter1.js
@@ -1,8 +1,10 @@
 let products = [
-    {name : 'cucumbers', type : 'vegetable'},
-    {name : 'apple', type : 'fruit'},
-    {name : 'mango', type : 'fruit'},
-    {name : 'milk', type : 'drinks'}
+    {name : 'cucumbers', type : 'vegetable', quantity : 1, price : 15},
+    {name : 'apple', type : 'fruit',quantity : 1, price : 5},
+    {name : 'mango', type : 'fruit',quantity : 5, price : 20},
+    {name : 'carrot', type : 'vegetable', quantity : 2, price : 12},
+    {name : 'tomato', type : 'vegetable', quantity : 0, price : 2},
+    {name : 'milk', type : 'drinks',quantity : 3, price : 1}
 ];
 
 let result = products.filter((product) => {
@@ -10,3 +12,12 @@ let result = products.filter((product) => {
 });
 
 console.log(result);
+
+/**************************************************************************************************/
+
+let result2 = products.filter((product) => {
+    return product.type === 'vegetable' && product.quantity > 0 && product.price > 10;
+});
+
+console.log('\n\n\n\n');
+console.log(result2);
