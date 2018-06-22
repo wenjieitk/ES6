9ca1ede1a75c192d8fc98603235e6cdcfaa68645
Wen Jie <wenjie@cavman.com.my>
2018-06-22 18:56:55 +0800

    find helper with class function

--- find/find1.js
+++ find/find1.js
@@ -12,3 +12,26 @@ let result = users.find((user) => {
 });
 
 console.log(result);
+
+
+/*****************************************************************************************/
+
+/******************************** */
+
+function Car(model) {
+    this.model = model;
+}
+
+var cars = [
+    new Car ('Buick'),
+    new Car('Camaro'),
+    new Car('Focus'),
+    new Car('Buick')
+];
+
+let result2 = cars.find((car) => {
+    return car.model === 'Buick';
+});
+
+
+console.log(result2);
