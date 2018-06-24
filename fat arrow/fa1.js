6b273ab73c61286eacfcbaa59fafec9fe8744f05
Wen Jie <wenjie@cavman.com.my>
2018-06-25 04:21:50 +0800

    fat arrow theory

---
+++ fat arrow/fa1.js
@@ -0,0 +1,26 @@
+// single expression 
+// single argument
+const add = a => a + 2;  // add (a) { return a+2 }
+
+
+// arrow function does not work in this situation
+const profile = {
+    name: 'Alex',
+    getName: function (name)  {
+        return this.name
+    }
+};
+
+//arrow function solve this situation
+const team = {
+    members: ['slex','dfdf'],
+    teamName: 'abc',
+    teamSum: function ( )  {
+        return this.members.map(member =>  `${member} is on team ${this.teamName}`); //solve the type error in map
+    }
+};
+
+
+console.log(team.teamSum());
+
+
