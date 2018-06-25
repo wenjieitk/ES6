53c2cb8687bfbe351d4544363eb3cab2f3c046d8
Wen Jie <wenjie@cavman.com.my>
2018-06-25 16:56:54 +0800

    enhanced object literals

---
+++ object literals/ol1.js
@@ -0,0 +1,11 @@
+let obj = {
+    //inventory : inventory
+    inventory,
+
+    // inventoryValue: function(){
+    //     return this.value;
+    // }
+    inventoryValue(){
+        return this.value;
+    }
+}
