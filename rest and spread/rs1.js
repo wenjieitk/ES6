4b4d09f8109764d68fa0df8c913fa96d6ff30180
Wen Jie <wenjie@cavman.com.my>
2018-06-25 19:29:15 +0800

    rest and spread 2

--- rest and spread/rs1.js
+++ rest and spread/rs1.js
@@ -2,6 +2,8 @@
  *  ...variable
  */
 
+
+ // rest
  const addNumbers = (...numbers) => {
      return numbers.reduce((sum,number) => {
          return sum+number;
@@ -12,3 +14,13 @@
     addNumbers(1,2,3,4,5,6,7,8)
  );
  
+
+ //spread
+
+ const defaultColors = ['red', 'green'];
+ const favorColors = ['orange','yellow'];
+ const fallColors = ['fire red','fall orange'];
+
+ console.log(
+     ['blue', ...fallColors,...favorColors,...fallColors]
+ ); //[ 'blue','fire red','fall orange','orange','yellow','fire red','fall orange' ]
