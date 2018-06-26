e2c65a8f1c5a9f5191bf5a15737ab89500224892
Wen Jie <wenjie@cavman.com.my>
2018-06-26 19:25:14 +0800

    array -> obj using destructuring

---
+++ destructuring/d4.js
@@ -0,0 +1,17 @@
+/**
+ * convert array to object 
+ * [4,5] -> {x:4,y:5}
+ */
+
+const points = [
+    [4,5],
+    [10,1],
+    [0,40]
+];
+
+let obj = points.map(([x,y])=>{
+    return {x,y};
+});
+
+console.log(obj);
+//[ { x: 4, y: 5 }, { x: 10, y: 1 }, { x: 0, y: 40 } ]
