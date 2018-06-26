6e19d9133d4b5744113f3d0997ca4acd5f282d93
Wen Jie <wenjie@cavman.com.my>
2018-06-26 17:24:13 +0800

    destructure 2

--- destructuring/d1.js
+++ destructuring/d1.js
@@ -9,3 +9,30 @@ let amount5 = expense.amount;
 
 //es6
 const {type,amount} = expense;
+
+/***********************************************************************************/
+
+let savedFiled = {
+    extension: 'jpg',
+    name: 'report',
+    size: 14040
+};
+
+//es5
+function fileSummary5 (obj) {
+    return `the file ${obj.name}.${obj.extension} is of size ${obj.size}`;
+}
+
+console.log(
+    fileSummary5(savedFiled)
+);
+
+
+//es6
+function fileSummary6({name,size,extension}, {anotherFile}) {
+    return `the file ${name}.${extension} is of size ${size} and got 1 ${anotherFile}`;
+}
+
+console.log(
+    fileSummary6(savedFiled,{anotherFile : 'extra'})
+);
