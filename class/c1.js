efc67b185ec7fc141297263fd5be47ce030ce724
Wen Jie <wenjie@cavman.com.my>
2018-06-27 17:35:24 +0800

    inheritance

--- class/c1.js
+++ class/c1.js
@@ -4,16 +4,36 @@ class Car {
         this.title = title;
     }
 
-
     //function
     drive () {
         return 'vroom';
     }
 }
 
+/**
+ * inheritance from Car
+ * super() -> inherit parent; cannot user destructure
+ */
+class Toyota extends Car { 
+
+    constructor(options) {
+        super(options); // Car.constructor -> car.title
+        this.color = options.color;
+    }
+
+    honk() {
+        return 'beep from Toyota';
+    }
+}
+
+
 const car = new Car({title:'Toyota'});
+const toyota = new Toyota({color : 'red', title: 'daily drive'}); // inject 'title' which toyota dun have in constructor
 
 console.log(
     car,
-    car.drive()
+    car.drive(),
+    toyota, 
+    toyota.honk(),
+    toyota.drive() // Car's func, toyota dun have it
 )
