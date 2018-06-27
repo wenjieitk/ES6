7f211f8f7a6751954491945764c4e45f28ac70a8
Wen Jie <wenjie@cavman.com.my>
2018-06-27 21:38:31 +0800

    iterator usecase 2

--- generators/g2.js
+++ generators/g2.js
@@ -11,8 +11,32 @@ function* colors() {
 
 
 const arrColors = [];
-for(let color of colors()){
-    arrColors.push(color);
+for(let color of colors()){ // execute iterator function here
+    arrColors.push(color); // store result here
 }
 
 console.log(arrColors);
+
+/*********************************************************************************/
+// to get all the member name from ourTeam
+
+const ourTeam = {
+    size : 3,
+    location : 'mmu',
+    lead : 'Jill',
+    manager : 'Alex',
+    engineer : 'Dave'
+};
+
+function* TeamIterator(team) {
+    yield team.lead;
+    yield team.manager;
+    yield team.engineer;
+}
+
+const names = [];
+for (let name of TeamIterator(ourTeam)){ // execute iterator function here
+    names.push(name); // store result here
+};
+
+console.log(names);
