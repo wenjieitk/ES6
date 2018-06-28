b35bf7e6bf3ac08dc408f309c79fb266769cbe63
Wen Jie <wenjie@cavman.com.my>
2018-06-28 20:20:40 +0800

    delegation with generator (Symbol.iterator)

--- generators/g2.js
+++ generators/g2.js
@@ -19,8 +19,20 @@ console.log(arrColors);
 
 /*********************************************************************************/
 // to get all the member name from ourTeam
+// Symbol.iterator - to load other team 
+
+const theirTeam = {
+    lead: 'Amanda',
+    tester: 'Bill',
+    // Symbol.iterator to tell TeamIterator which to yield here
+    [Symbol.iterator]: function* () {
+        yield this.lead;
+        yield this.tester;
+    }
+};
 
 const ourTeam = {
+    theirTeam, //theirTeam (1)
     size : 3,
     location : 'mmu',
     lead : 'Jill',
@@ -32,6 +44,7 @@ function* TeamIterator(team) {
     yield team.lead;
     yield team.manager;
     yield team.engineer;
+    yield* team.theirTeam; //theirTeam (2) - yield everything in theirTeam
 }
 
 const names = [];
