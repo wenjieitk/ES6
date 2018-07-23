3eda577b8011f14d89dbe11ad393b214952800a3
Wen Jie <wenjie@cavman.com.my>
2018-07-23 21:57:07 +0800

    abstract and scope

--- this/this1.js
+++ this/this1.js
@@ -41,3 +41,34 @@ const video = {
 }
 
 video.showTags();
+
+/**
+ * abstract and scope
+ * let in function = private
+ */
+
+ function cc(r) {
+     this.radius = radius;
+     
+     let defaultLocation = {
+         x: 0,
+         y: 0
+     };
+
+     let computeLocation = function(f) {
+         console.log('inside compute ',f);
+         
+     }
+
+     this.draw = function() {
+        // calling private function
+        computeLocation(1);
+        //calling private variable
+        defaultLocation.x +=1;
+        //calling cc variable
+        this.radius +=1;
+     }
+ }
+
+ const newcc = new cc();
+ // only radius and draw can be access from outside
