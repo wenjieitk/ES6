3d8927697e7b2d12da754d7a3fe1a627c2d197db
Wen Jie <wenjie@cavman.com.my>
2018-08-06 19:38:50 +0800

    promises update

--- promise and fetch/p1.js
+++ promise and fetch/p1.js
@@ -8,3 +8,23 @@ promise
     .then(() => console.log('also finished'))
     .then(() => console.log('finally finished'))
     .catch(() => console.log('this is error'))
+
+
+/*************************************************/
+
+getUser(1)
+    .then((user) => {
+        console.log(user);
+    })
+
+function getUser(id) {
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            console.log('inside async....');
+            resolve({
+                id: id,
+                username: "wen jie"
+            })
+        }, 1500);
+    })
+}
